90913f30eb1553e63d5ff2ab4945c4303fec571b
Gift Egwuenu <laurandidi21@gmail.com>
2020-05-27 20:09:37 +0100

    add fancy blog

---
+++ src/components/PostList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="post-list">
+    <hr class="line">
+    <p class="date" v-html="post.date"/>
+    <h3 class="title" v-html="post.title"/>
+    <p class="description" v-html="post.description"/>
+    <b>{{post.timeToRead}} min read</b> &nbsp;
+    <g-link :to="post.path" class="read">Read More <span class="visuallyhidden">about {{post.title}}</span></g-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["post"]
+};
+</script>
+
+<style>
+.line {
+  border: 0.5px solid #cdc8c5;
+  margin: 30px 0;
+}
+
+.date {
+  font-weight: 300;
+}
+
+.read {
+  padding: 7px;
+  color: #333;
+  text-decoration: none;
+  border: 2px dashed #fecd43;
+}
+
+.visuallyhidden {
+  position: absolute !important;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  clip-path: inset(50%);
+  border: 0;
+}
+.visuallyhidden:focus {
+  display: inline-block;
+  padding: 15px 23px 14px;
+  width: auto;
+  height: auto;
+  clip: auto;
+  color: #000000;
+  background-color: #ffffff;
+  clip-path: unset;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,61 @@
+<template>
+  <Layout>
+    <header class="header">
+      <h1 v-html="$page.metaData.siteName" />
+      <h2 v-html="$page.metaData.siteDescription" />
+    </header>
+    <section class="posts">
+      <PostList v-for="edge in $page.allPost.edges" :key="edge.node.id" :post="edge.node" />
+    </section>
+  </Layout>
+</template>
+
+<script>
+import PostList from "@/components/PostList";
+export default {
+  components: {
+    PostList
+  },
+  metaInfo: {
+    title: "A simple blog"
+  }
+};
+</script>
+
+<page-query>
+query {
+  metaData {
+    siteName
+    siteDescription
+  }
+  allPost {
+    totalCount
+    edges {
+      node {
+        id
+        title
+        timeToRead
+        description
+        date (format: "D MMMM YYYY")
+        path
+      }
+    }
+
+  }
+}
+</page-query>
+
+<style>
+.header {
+  font-family: "Stylish";
+  font-size: 35px;
+  text-align: center;
+  line-height: 20px;
+  padding: 0.7em;
+}
+
+.header h2 {
+  font-weight: 200;
+  font-size: 35px;
+}
+</style>
